7feaf7cddd52ef5fe0c6845fc87f91f47c19fef1
Pranav <kamblepranav460@gmail.com>
2024-07-15 22:44:05 +0530

    vite conf updated

--- vite.config.mjs
+++ vite.config.mjs
@@ -43,7 +43,7 @@ export default defineConfig(() => {
       port: 3000,
       proxy: {
         '/api': {
-          target: 'https://dev.tourkokan.com/admin/v2', // Your backend API server
+          target: 'http://dev.tourkokan.com/admin/v2', // Your backend API server
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix from path
           secure: true // Make sure requests are secure (HTTPS)
